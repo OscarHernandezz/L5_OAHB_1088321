624ee83fe42a324450610097366d68a48cd38f2a
Alexa <Alexa@LAPTOP-PS14S1BS>
2022-09-27 13:34:52 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace L5_OAHB_1088321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int num;
+            Console.WriteLine("Ejercicio 1");
+            Console.WriteLine("Ingrese un Número entero: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+           
+                if (num < 0)
+                {
+                    Console.WriteLine("Número negativo");
+
+                }
+                else if (num == 0)
+                {
+                    Console.WriteLine("El número es cero");
+                }
+                else if (num > 0)
+                {
+                    Console.WriteLine("Número positivo");
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un número entero");
+                }
+            
+                Console.WriteLine("Ingrese un número");
+                
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
